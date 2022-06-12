81037f74d3fb889036c5a961b49f10311bf4791d
Marian Keck <mariankeck@gmail.com>
2022-06-12 08:26:45 +0000

    RMSE calculation implemented

--- src/tools.cpp
+++ src/tools.cpp
@@ -3,17 +3,38 @@
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
+using std::cout;
+using std::endl;
 using std::vector;
 
 Tools::Tools() {}
 
 Tools::~Tools() {}
 
-VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth) {
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // Check the validity of the inputs
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+    cout << "Invalid estimation or ground truth data" << endl;
+    return rmse;
+  }
+
+  // accumulate squared residuals
+  for (unsigned char i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+
+    // Coefficient-wise multiplication
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  // Calculate the mean and square root
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+  
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
