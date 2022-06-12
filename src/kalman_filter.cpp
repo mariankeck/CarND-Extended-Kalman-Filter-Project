ccca3013b6a810dfd6cbf2da87c077bce263197c
Marian Keck <mariankeck@gmail.com>
2022-06-12 23:30:26 +0300

    Init method removed and EKF update implemented

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -12,32 +12,14 @@ KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
-
-  long x_size = x_.size();
-  I_ = MatrixXd::Identity(x_size, x_size);
-}
-
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
   x_ = F_ * x_;
   P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
-  VectorXd y = z - H_ * x_;
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd K = P_ * Ht * S.inverse();
@@ -47,7 +29,18 @@ void KalmanFilter::Update(const VectorXd &z) {
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  // Calculate measurement function h
+  float rho = sqrt(x_[0] * x_[0] + x_[1] * x_[1]);
+  float phi = atan2(x_[1], x_[0]);
+  float rho_dot = fabs(rho) < 0.0001 ? 0 : x_[0] * x_[2] + x_[1] * x_[3] / rho;
+
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd K = P_ * Ht * S.inverse();
+
+  x_ = x_ + K * y;
+  P_ = (I_ - K * H_) * P_;
 }
